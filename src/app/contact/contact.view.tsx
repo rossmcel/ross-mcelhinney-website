bca8becaef2900e91a743c026e39c5e7dc1c6bc9
rossmcel <rossmcelhinney123@gmail.com>
2021-07-15 15:45:47 +0100

    Remove Header Section from Contacts Page

--- src/app/contact/contact.view.tsx
+++ src/app/contact/contact.view.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Box, Flex, Center } from "@chakra-ui/react";
-import DynamicTitleSection from "../../shared/components/dynamic-title-section/dynamicTitleSection";
 import { AnimatedEmailIcon } from "../../shared/components/animated-email-icon";
 import { AnimatedPhoneIcon } from "../../shared/components/animated-phone-icon";
 import { AnimatedLinkedinIcon } from "../../shared/components/animated-linkedin-icon";
@@ -8,7 +7,6 @@ import { AnimatedLinkedinIcon } from "../../shared/components/animated-linkedin-
 export const ContactView: React.FC = () => {
   return (
     <Box overflow="hidden" minH="130rem" mt="2rem">
-      <DynamicTitleSection />
       <Flex
         direction="row"
         mt="5rem"
