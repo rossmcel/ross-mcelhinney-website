7cc2a92d9444d9dae03f8ac77b7777cd88e8665a
rossmcel <rossmcelhinney123@gmail.com>
2021-07-18 20:47:10 +0100

    Add More Content to Projects Page

--- src/shared/components/left-right-image-text/leftRightImageText.view.tsx
+++ src/shared/components/left-right-image-text/leftRightImageText.view.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Box, Heading, Text, Flex, Image } from "@chakra-ui/react";
+import { Box, Heading, Text, Flex, Image, Button } from "@chakra-ui/react";
 import ScrollAnimation from "react-animate-on-scroll";
 import HomePageRecording from "../../../homePageRecordingMid.gif";
 import { LaptopDisplay } from "../device-displays/laptop-display";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 export const LeftRightImageTextView: React.FC = () => {
   return (
@@ -44,7 +45,8 @@ export const LeftRightImageTextView: React.FC = () => {
                 Contact Me
               </a>{" "}
               page. By creating these icons, I discovered a lot about animation
-              in React as well as the extensive amount of helpful npm packages.
+              in React as well as about the extensive amount of helpful npm
+              packages.
             </Text>
             <Text m="1rem">
               Although this was not a group project, I developed with
@@ -52,6 +54,12 @@ export const LeftRightImageTextView: React.FC = () => {
               code, effective seperation of components, strong documentation and
               my use of Git.
             </Text>
+            <a href="https://github.com/rossmcel">
+              <Button bgColor="blue" color="white" fontSize="sm" ml="1rem">
+                See the GitHub Repo{" "}
+                <ExitToAppIcon style={{ marginLeft: "0.5rem" }} />
+              </Button>
+            </a>
           </Box>
         </Flex>
       </Flex>
@@ -74,7 +82,28 @@ export const LeftRightImageTextView: React.FC = () => {
         </Box>
         <Flex direction="row" mt="1rem" minW="100%" minH="100%">
           <Box minW="60%" maxW="60%">
-            <Text m="1rem">Web app</Text>
+            <Text m="1rem">
+              This project was my first venture into backend technologies. The
+              project stemmed from my desire to add a feature to a 1st Year Web
+              Development assignment that was outside the scope of the class.
+            </Text>
+            <Text m="1rem">
+              Through the use of primarily PHP and MySql, I created a
+              functioning web application capable of storing, sorting and
+              displaying user entered data in many different forms.
+            </Text>
+            <Text m="1rem">
+              The project was driven by my interest in and desire to learn more
+              about programming. It furthered my development skill greatly and
+              has provided me with a helpful codebase that provides real-world
+              examples of the use of different technologies.
+            </Text>
+            <a href="https://github.com/rossmcel">
+              <Button bgColor="blue" color="white" fontSize="sm" ml="1rem">
+                See the GitHub Repo{" "}
+                <ExitToAppIcon style={{ marginLeft: "0.5rem" }} />
+              </Button>
+            </a>
           </Box>
           <ScrollAnimation animateIn="fadeIn" animateOnce={true} duration={5}>
             <LaptopDisplay screenImage={HomePageRecording} />
