35b4d1319a831e5369a48a998bbaf3d99b3d11e4
rossmcel <rossmcelhinney123@gmail.com>
2021-08-11 22:23:17 +0100

    Fix LeftRightImageText Component Rendering Bug

--- src/shared/components/left-right-image-text/leftRightImageText.view.tsx
+++ src/shared/components/left-right-image-text/leftRightImageText.view.tsx
@@ -44,8 +44,8 @@ export const LeftRightImageTextView: React.FC<LeftRightImageTextViewProps> = ({
             <Box
               minW={{ base: "95%", lg: "60%" }}
               maxW={{ base: "95%", lg: "60%" }}
-              ml={{ base: "auto", lg: "intial" }}
-              mr={{ base: "auto", lg: "intial" }}
+              ml={{ base: "auto", lg: "initial" }}
+              mr={{ base: "auto", lg: "initial" }}
             >
               {children}
             </Box>
@@ -60,7 +60,7 @@ export const LeftRightImageTextView: React.FC<LeftRightImageTextViewProps> = ({
           ml="5%"
           mr="5%"
         >
-          <Box ml="60%">
+          <Box ml={{ base: "5%", lg: "60%" }}>
             <Heading as="h3" size="lg" letterSpacing="-.1rem">
               {heading}
             </Heading>
@@ -68,11 +68,30 @@ export const LeftRightImageTextView: React.FC<LeftRightImageTextViewProps> = ({
               {subheading}
             </Text>
           </Box>
-          <Flex direction="row" mt="1rem" minW="100%" minH="100%">
-            <Box minW="60%" maxW="60%">
+          <Flex
+            direction={{ base: "column", lg: "row" }}
+            display={{ base: "grid", lg: "initial" }}
+            mt="1rem"
+            minW="100%"
+            minH="100%"
+          >
+            <Box
+              gridRow={{ base: "2", lg: "none" }}
+              gridColumn={{ base: "1", lg: "none" }}
+              minW={{ base: "95%", lg: "60%" }}
+              maxW={{ base: "95%", lg: "60%" }}
+              ml={{ base: "auto", lg: "initial" }}
+              mr={{ base: "auto", lg: "initial" }}
+            >
               {children}
             </Box>
-            {nonTextContent}
+            <Box
+              display={{ base: "block", lg: "none" }}
+              gridRow="1"
+              gridColumn="1"
+            >
+              {nonTextContent}
+            </Box>
           </Flex>
         </Flex>
       )}
